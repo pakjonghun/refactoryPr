ca8fc0cb166f8c65878a49f3bb0688a728760a6e
jong hun pak <fireking5997@gmail.com>
2022-12-18 13:21:27 +0900

    fix:클래스 질의 함수로 메서드로 빼내기

--- refactoring-before/ch7/7-4.js
+++ refactoring-before/ch7/7-4.js
@@ -6,10 +6,19 @@ class Order {
     this.#item = item;
   }
 
+  get basePrice() {
+    return this.#quantity * this.#item.price;
+  }
+
+  get discountFactor() {
+    this.basePrice > 1000 //
+      ? (this.discountFactor -= 0.03)
+      : 0.98;
+  }
+
   get price() {
-    const basePrice = this.#quantity * this.#item.price;
-    const discountFactor = 0.98;
-    if (basePrice > 1000) discountFactor -= 0.03;
-    return basePrice * discountFactor;
+    //Note:필요 재활용 한다면 인라인 한 것을 다시 함수로 빼야 하는데 이것을 질의함수로 빼면된다
+    // 위의 2개 getter 는 모두 price 에 있던 인라인 변수 들임.
+    return this.basePrice * this.discountFactor;
   }
 }
